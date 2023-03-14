5e8beb6d872fd0c1dacd030147f60aec6e58fad1
user <nandaras0103@gmail.com>
2023-03-14 14:41:24 +0900

    high arrays

--- src/arrays_02_03/HighArray.java
+++ src/arrays_02_03/HighArray.java
@@ -2,4 +2,69 @@ package arrays_02_03;
 
 public class HighArray {
 
+	private int[] arr;    // refer to array
+	private int count;    //number of data items
+	
+	public HighArray (int max) {
+		arr = new int[max];
+		count = 0;
+	}
+	
+	
+	// insert items into array
+	public void insert(int item) {
+		arr[count] = item;
+		count++;
+	}
+	
+	
+	// find specified values with using boolean
+	public boolean findItem(int item) {
+		int i;
+		for(i = 0; i < count; i++) {
+			if(arr[i] == item) 
+				break;
+			}
+			
+			if(i == count) 
+				return false;
+			else
+				return true;
+	}
+	
+
+	
+	// delete items with using boolean
+	public boolean delete(int item)
+	{
+		int i;
+		for(i = 0; i < count; i++) {
+			if(arr[i] == item) 
+				break;
+		}
+		
+		if(i == count) 
+			return false;
+			
+		for(int k = i; k < count-1; k++)  //count = 10
+				arr[k] = arr[k+1];
+		count--;
+		return true;
+		
+	}
+	
+	// display the items
+	public void print() {
+		System.out.println();
+		for(int i = 0; i < count; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
 }
+
+
+
+
+
--- src/arrays_02_03/HighArrayApps.java
+++ src/arrays_02_03/HighArrayApps.java
@@ -1,5 +1,30 @@
 package arrays_02_03;
-
+// demonstrate array class with high-level interface to run this program
 public class HighArrayApps {
 
+	public static void main(String[] args) {
+		
+		int maxSize = 100;
+		HighArray arr = new HighArray(maxSize);
+		
+		arr.insert(10);
+		arr.insert(20);
+		arr.insert(30);
+		arr.insert(40);
+		arr.insert(50);
+		arr.insert(60);
+		arr.insert(70);
+		arr.insert(80);
+		arr.insert(90);
+		
+		arr.delete(30);
+		
+		if(arr.findItem(90))
+			System.out.println("find the itme ");
+		else {
+			System.out.println("Cannot find the item");
+		}
+		
+		arr.print();
+	}
 }
